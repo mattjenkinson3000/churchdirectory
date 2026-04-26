e998949a7d9cd61b53b66a2020e66e55a2956d6f
Matt Jenkinson <mattjenkinson3000@users.noreply.github.com>
2026-04-26 21:33:58 +1200

    Fix image domains with wildcard for all Google subdomains

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
+        hostname: '**.googleusercontent.com',
         pathname: '/**',
       },
       {
         protocol: 'https',
-        hostname: 'lh4.googleusercontent.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh5.googleusercontent.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'streetviewpixels-pa.googleapis.com',
+        hostname: '**.googleapis.com',
         pathname: '/**',
       },
     ],
